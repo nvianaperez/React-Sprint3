b40652a5df91eabe501c157da49096015ed541fe
Nuria Viana <nvianaperez@gmail.com>
2021-10-26 07:57:46 +0200

    React - Sprint 3 - entrega

--- js/checkout.js
+++ js/checkout.js
@@ -1,14 +1,127 @@
+/*
+
+* minlenght attribute in html5 does not provide any feedback, so you can write js code
+* Validación de HTML5 pattern="/^[A-Z]+$/i" and title="alphabet only case-insensitive", 
+or through regular expression, donde:
+^...$ indica que el patrón debe coincidir exactamente con los caracteres dentro de los corchetes,
+[A-Z] indica que los caracteres admitidos son letras del alfabeto (minusc y mayusc),
++ indica que los caracteres dentro de los corchetes se pueden repetir,
+i indica que validaremos letras mayúsculas y minúsculas (case-insensitive)
+Example:
+<input class="ml-3 phone input rounded p-2" type="tel" placeholder="Phone No." id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}">
+*
+*/
+
 // Get the input fields
-var password = document.querySelector(".password");
-var phone = document.querySelector('.phone');
-var name = document.querySelector('.name');
+var formulario = document.getElementById('formulario'); // acceder al formulario completo
+var inputs = document.querySelectorAll('#formulario input'); // acceder a todos los elementos (array inputs) con class formulario y element input
+
+var firstName = document.getElementById('firstName').value; // obtener el valor del id="firstName"
+var lastName = document.getElementById('lastName').value;
+var email = document.getElementById('email').value;
+var address = document.getElementById('address').value;
+var password = document.getElementById('password').value;
+var phone = document.getElementById('phone').value;
 
 // Get the error elements
-var errorPassword = document.getElementById("errorPassword");
-var errorName = document.getElementById('errorName');  
-var errorPhone = document.getElementById('errorPhone');  
+var errorFirstName = document.getElementById('errorFirstName');
+var errorLastName = document.getElementById('errorLastName');
+var errorPassword = document.getElementById('errorPassword');
+var errorPhone = document.getElementById('errorPhone');
+var errorEmail = document.getElementById('errorEmail');
 
-// Exercise 9
-function validate() {
-    // Validate fields entered by the user: name, phone, password, and email
+var expresionesRegulares = {
+    nombre: /^[a-zA-Z]+$/, // alphabet only case-insensitive.
+    telefono: /^\d{9,14}$/, // only numbers.
+    password: /^[a-zA-Z0-9]{3,14}$/, // only alphabet and numbers.
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 }
+
+var campos = {
+    firstName: false,
+    lastName: false,
+    password: false,
+    email: false,
+    address: false,
+    phone: false,
+}
+
+var validarFormulario = (e) => {
+    switch (e.target.name) {
+        case 'firstName':
+            checkCampo(expresionesRegulares.nombre, e.target, 'firstName', 'errorFirstName', 'errorCompleteFirstName');
+            break;
+        case 'lastName':
+            checkCampo(expresionesRegulares.nombre, e.target, 'lastName', 'errorLastName', 'errorCompleteLastName');
+            break;
+        case 'password':
+            checkCampo(expresionesRegulares.password, e.target, 'password', 'errorPassword', 'errorCompletePassword');
+            break;
+        case 'email':
+            checkCampo(expresionesRegulares.correo, e.target, 'email', 'errorEmail', 'errorCompleteEmail');
+            break;
+        case 'phone':
+            checkCampo(expresionesRegulares.telefono, e.target, 'phone', 'errorPhone', 'errorCompletePhone');
+            break;
+        default:
+            checkAddress(e.target, 'address', 'errorCompleteAddress');
+    }
+}
+
+var checkCampo = (expresion, input, campo, error1, error2) => {
+    if (expresion.test(input.value) && input.value != " " && input.value.indexOf(" ") == -1 && input.value.length >= 3) {
+        document.getElementById(`${campo}`).classList.add('formulario_input-validate');
+        document.getElementById(`${campo}`).classList.remove('formulario_input-noValidate');
+        document.getElementById(`${error1}`).style.display = "none";
+        document.getElementById(`${error2}`).style.display = "none";        
+        // document.querySelector(`${campo} p`).style.display="none";
+    } else {
+        document.getElementById(`${campo}`).classList.add('formulario_input-noValidate');
+        document.getElementById(`${error1}`).style.display = "block";
+        document.getElementById(`${error2}`).style.display = "block";
+        // document.querySelector(`${campo} p`).style.display="block";
+    }
+}
+
+var checkAddress = (input, campo, error2) => {
+    if (input.value != " " && input.value.indexOf(" ") == -1 && input.value.length >= 3) {
+        document.getElementById(`${campo}`).classList.add('formulario_input-validate');
+        document.getElementById(`${campo}`).classList.remove('formulario_input-noValidate');
+        document.getElementById(`${error2}`).style.display = "none";        
+    } else {
+        document.getElementById(`${campo}`).classList.add('formulario_input-noValidate');
+        document.getElementById(`${error2}`).style.display = "block";
+    }
+}
+
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', validarFormulario);
+    input.addEventListener('blur', validarFormulario);
+});
+/*por cada input añade un eventListener: cuando levanto la tecla y cuando clico fuera del formulario, 
+se ejecuta la función validarFormulario*/
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+/*cuando presiono boton que es type submit --> se llama a una función tipo arrow
+en la función evento se pasa por parámetro y se ejecuta preventDefault()
+preventDefault() no guarda nada, la página no cambia*/
+
+
+/*
+    // Validate var phone
+    phone = parseInt(phone); //intento convertir a entero el input de la var phone. Si lo consigue, ya está. Si no lo consigue devuelve NaN
+    //Compruebo si es un valor numérico
+    if (isNaN(phone) || phone.toString().length < 3) {
+        //entonces (no es numero) y devuelve mensaje de error
+        return alert(errorPhone);
+    } else {
+        //En caso contrario (Si era un número) devuelvo el valor
+        return phone;
+    }
+    // .length solo lee strings
+    // typeof phone !== number --> usuario puede entrar números entre comillas..
+*/
+
--- js/grocery.js
+++ js/grocery.js
@@ -51,73 +51,276 @@ var cartList = [];
 var cart = [];
 var subtotal = {
     grocery: {
-        value: 0, 
+        value: 0,
         discount: 0
     },
     beauty: {
-        value: 0, 
+        value: 0,
         discount: 0
     },
     clothes: {
-        value: 0, 
+        value: 0,
         discount: 0
     },
 };
 var total = 0;
 
+
 // Exercise 1
-function addToCartList(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+function addToCart(id) {
+    // 1. Loop for to the array products to get the item to add to cartList
+    for (let i = 0; i < products.length - 1; i++) {
+        if (id === products.indexOf(products[i])) {
+            cartList.push(products[i]); // 2. Add found product to the cartList array
+        }
+    }
+    console.log(cartList);
 }
+
+// // Exercise 9
+// function removeFromCart(id) {
+//     for (let i = 0; i < cartList.length - 1; i++) {
+//         if (products[id].name === cartList[i].name) {
+//            cartList.splice(cartList.indexOf(cartList[i]),1);
+//         }
+//     }
+//     console.log(cartList);
+// }
 
 // Exercise 2
 function cleanCart() {
-
+    cartList = [];
+    console.log(cartList);
 }
 
-// Exercise 3
-function calculateSubtotals() {
+// Exercise 3 - SUBTOTALS IN $ - object subtotals
+function calculateSubtotalsValue() {
     // 1. Create a for loop on the "cartList" array 
     // 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
+
+    // limpiar el objeto subtotal, sin var (porque con var subtotal lo que hago es crear una segunda variable subtotal vacía y todo lo demas lo haría sobre esta vacía)
+    subtotal = {
+        grocery: {
+            value: 0,
+            discount: 0
+        },
+        beauty: {
+            value: 0,
+            discount: 0
+        },
+        clothes: {
+            value: 0,
+            discount: 0
+        },
+    };
+
+    // llamar a la función que genera cart, a partir de cartList
+    generateCart(cartList);
+
+    // recorrer cart buscando el tipo de producto y rellenando el valor del objeto subtotal, en la categoría que le corresponda.
+    for (let i = 0; i < cart.length; i++) {
+        switch (cart[i].type) {
+            case 'grocery':
+                subtotal.grocery.value += cart[i].subtotal;
+                break;
+            case 'beauty':
+                subtotal.beauty.value += cart[i].subtotal;
+                break;
+            case 'clothes':
+                subtotal.clothes.value += cart[i].subtotal;
+                break;
+            default:
+                console.log('Error: el producte no té categoria');
+                break;
+        }
+    }
+    console.log(subtotal);
 }
 
-// Exercise 4
+// Exercise 4a - bucle for
+// function calculateTotal() {
+//     // Calculate total price of the cartList either using the "cartList" array
+//     total = 0; // inicializo en 0 la variable global total
+//     for (let i = 0; i < cartList.length; i++) {
+//         total += cartList[i].price;
+//     }
+//     console.log(total);
+// }
+// Exercise 4 - forEach
+// function calculateTotal() {
+//     total = 0; // inicializo en 0 la variable global total
+//     cartList.forEach(product => {
+//         total += product.price
+//     });
+//     console.log(total);
+// }
+
+// **traer objeto subtotal rellenado de la función anterior, recorrerlo sumando los subtotales para dar la variable total.
 function calculateTotal() {
-    // Calculate total price of the cart either using the "cartList" array
+    console.log(subtotal);
+    for (type in subtotal) {
+        total += subtotal[type].value;
+    }
+    console.log(total);
+
 }
 
-// Exercise 5
-function applyPromotionsSubtotals() {
+// function calculateTotal() {
+//     for (let i = 0; i < cart.length; i++) {
+//         total += cart[i].subtotal;
+//     }
+//     console.log(total);
+// }
 
-}
+
+// // Exercise 5
+// function applyPromotionsSubtotals() {
+// // aquest exercicino hi és a l'enunciat de la tasca
+// }
 
 // Exercise 6
-function generateCart() {
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
-    // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+function generateCart(cartList) {
+    // Using the "cartlist" array that contains all the items in the shopping cartList, 
+    // generate the "cartList" array that does not contain repeated items, instead each item of this array "cartList" shows the quantity of product.
+    cart = [];
+    let i = 0;
+    let j = 0;
+    cartList.sort((a, b) => (a.name > b.name) ? -1 : 1); //ordena el cartList que recibo por orden alfabético de nombre
+    // console.log(cartList);
+    for (i = 0; i < cartList.length; i++) {
+        cartList[i].quantity = 0;
+        cartList[i].subtotal = cartList[i].quantity * cartList[i].price;
+    }
+    if (cart.length === 0) {
+        cart.push(cartList[0]);
+    }
+
+    for (i = 0; i < cartList.length; i++) {
+        for (j = cart.length - 1; j < cart.length; j++) {
+            if (cartList[i].name === cart[j].name) {
+                cart[j].quantity = cartList[i].quantity + 1;
+                cartList[i].quantity = cart[j].quantity;
+                cart[j].subtotal = cart[j].quantity * cart[j].price;
+            } else {
+                cart.push(cartList[i]);
+                ++j;
+                cart[j].quantity = 1; //=cart[j]['quantity'] = 1;
+                cart[j].subtotal = cart[j].quantity * cart[j].price;
+            }
+        }
+    }
+    console.log(cart);
 }
 
 // Exercise 7
-function applyPromotionsCart() {
-    // Apply promotions to each item in the array "cart"
+function applyPromotionsCart(cart) {
+    // Apply promotions to each item in the array cart"
+    let pricePromCookingOil;
+    let pricePromCupcakeMixture;
+    //añado propiedad subtotalWithDiscount a todos los elementos de cart
+    for (let i = 0; i < cart.length; i++) {
+        cart[i].subtotalWithDiscount = cart[i].subtotal;
+    }
+    //aplico promociones a propiedad subtotalWithDiscount
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].name === 'cooking oil' && cart[i].quantity >= 3) {
+            pricePromCookingOil = 10;
+            cart[i].subtotalWithDiscount = cart[i].quantity * pricePromCookingOil;
+            console.log(`Subtotal with discount of Cooking oil promotion: ${cart[i].subtotalWithDiscount}`);
+        } else if (cart[i].name === 'Instant cupcake mixture' && cart[i].quantity >= 10) {
+            pricePromCupcakeMixture = Math.ceil(cart[i].price * (2 / 3));
+            cart[i].subtotalWithDiscount = cart[i].quantity * pricePromCupcakeMixture;
+            console.log(`Subtotal with discount of Instant cupcake mixture promotion: ${cart[i].subtotalWithDiscount}`);
+        } else
+            cart[i].subtotalWithDiscount = cart[i].subtotal;
+    }
+    //añado propiedad totalCart a todos los elementos de cart
+    for (let i = 0; i < cart.length; i++) {
+        cart[i].totalCart = cart[i].subtotalWithDiscount;
+    }
+    console.log(cart);
 }
 
-// Exercise 8
-function addToCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+// Exercise 8 lo comento para simplificar el programa
+// function addToCart(id) {
+// 1. Loop for to the array products to get the item to add to=cartList
+// 2. Add found product to the cartList array
+// addToCartList(id); 
+// }
+
+
+
+
+
+// // Exercise 10
+// function printCart() {
+//     // Fill the shopping=cartList modal manipulating the shopping=cartList dom
+// }
+
+
+// ************** otros ejercicios **************
+// Exercise 3a - SUBTOTALS IN $ - declare variables
+// function calculateSubtotalsValue() {
+//     // 1. Create a for loop on the "cartList" array 
+//     // 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
+//     let subtotalBeautyValue = 0;
+//     let subtotalGroceryValue = 0;
+//     let subtotalClothesValue = 0;
+//     for (let i = 0; i < cartList.length; i++) {
+//         switch (cartList[i].type) {
+//             case 'grocery':
+//                 subtotalGroceryValue += cartList[i].price;
+//                 break;
+//             case 'beauty':
+//                 subtotalBeautyValue += cartList[i].price;
+//                 break;
+//             case 'clothes':
+//                 subtotalClothesValue += cartList[i].price;
+//                 break;
+//             default:
+//                 console.log('Error: el producte no té categoria');
+//                 break;
+//         }
+//     }
+//     console.log(subtotalGroceryValue);
+//     console.log(subtotalBeautyValue);
+//     console.log(subtotalClothesValue);
+// }
+
+// Exercise 3b - SUBTOTALS IN UNITS
+function calculateSubtotalsUnits() {
+    // 1. Create a for loop on the "cartList" array 
+    // 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
+    let subtotalGrocery = [];
+    let subtotalBeauty = [];
+    let subtotalClothes = [];
+    for (let i = 0; i < cartList.length; i++) {
+        if (cartList[i].type === 'grocery') {
+            subtotalGrocery.push(cartList[i]);
+        } else if (cartList[i].type === 'beauty') {
+            subtotalBeauty.push(cartList[i]);
+        } else if (cartList[i].type === 'clothes')
+            subtotalClothes.push(cartList[i]);
+    }
+    console.log(`Subtotal Grocery = ${subtotalGrocery.length}`);
+    console.log(`Subtotal Beauty = ${subtotalBeauty.length}`);
+    console.log(`Subtotal Clothes = ${subtotalClothes.length}`);
 }
+//let subtotalGrocery = cartList.filter (cartList[i].type => cartList[i].type === 'grocery');
 
-// Exercise 9
-function removeFromCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
-}
+// //trepitja totes les propietats anterior, no serveix si el que volem és afegir quantity
+// for (let i = 0; i < cart.length; i++) {
+//    cart[i] = {
+//         quantity: 1
+//     }
+// }
 
-
-
-// Exercise 10
-function printCart() {
-    // Fill the shopping cart modal manipulating the shopping cart dom
-}
+//*************eliminar elementos repetidos de un array --> objeto Set
+// const cartUnique = new Set(cartList);
+// let result=[...cartUnique];
+// console.log(result);
+//*************eliminar elementos repetidos de un array --> método filter e indexOf
+// let result = (cartList.filter((item,index)=>{
+//   return cartList.indexOf(item) === index;
+// })
+// console.log(result);
